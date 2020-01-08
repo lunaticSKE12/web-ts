bb93f794f547af468aca4fc9b9b0af9e78f562d0
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-08 23:12:53 +0700

    Save set

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'new record', age: 1 });
 
-user.fetch();
+// user.set({ name: 'NEW NAME', age: 999 });
 
-setTimeout(() => {
-  console.log(user);
-}, 4000);
+user.save();
