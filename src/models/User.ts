3b3e2f3ca0f6764090a9f03baa22963b3aa5dd4c
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-08 21:36:27 +0700

    web

---
+++ src/models/User.ts
@@ -0,0 +1,28 @@
+interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    // this.events[eventName]; // Callback[] or indefined
+
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+}
