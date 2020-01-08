eccb277c65b955d5b8633799cf2b253f18a19a9e
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-08 21:39:21 +0700

    Callback test

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,9 @@ const user = new User({ name: 'myname', age: 20 });
 
 // console.log(user.get('name'));
 // console.log(user.get('age'));
+
+user.on('change', () => {});
+user.on('change', () => {});
+user.on('rebtbn', () => {});
+
+console.log(user);
