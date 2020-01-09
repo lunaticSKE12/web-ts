71c0601160d8b26f1b9740aa24f3be0c861c3ce6
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-09 23:19:02 +0700

    get

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,19 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 1 });
 
-user.attributes.get('id');
-user.attributes.get('name');
-user.attributes.get('age');
-user.sync.save();
+// A quick reminder on accessors
+
+// class Person {
+//   constructor(public firstName: string, public lastName: string) {}
+
+//   get fullname(): string {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
+
+// const person = new Person('firstname', 'lasename');
+// console.log(person.fullname);
+
+user.on('change', () => {
+  console.log('user was change');
+});
