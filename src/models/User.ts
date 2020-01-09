b8484fad6c6afb806717f697c79b098506db78b3
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-09 21:31:37 +0700

    Refactor sync attributes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Sync } from './Sync';
 import { Eventing } from './Eventing';
 
 export interface UserProps {
@@ -5,17 +6,11 @@ export interface UserProps {
   name?: string;
   age?: number;
 }
+const rootUrl = 'http://localhost:3000/users';
 
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
   constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
 }
