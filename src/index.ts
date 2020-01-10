76ecec468da7dde7f6e74b01617be8716cb659a5
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-10 23:13:14 +0700

    Strict null check

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import { User } from './models/User';
 import { UserForm } from './views/UserForm';
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
+const root = document.getElementById('root');
+if (root) {
+  const userForm = new UserForm(root, user);
 
-const userForm = new UserForm(document.getElementById('root'), user);
-
-userForm.render();
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
