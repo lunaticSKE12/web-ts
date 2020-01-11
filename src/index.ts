3f0ee1c7fdf8c24c45bba54b36d1fbade6ba8612
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-11 13:32:04 +0700

    Collection view

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserProps, User } from './models/User';
+import { Collection } from './models/Collection';
+import { UserList } from './views/UserList';
 
-const user = User.buildUser({ name: 'NAME', age: 20 });
-const root = document.getElementById('root');
-if (root) {
-  const userEdit = new UserEdit(root, user);
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-  userEdit.render();
-
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found');
-}
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+users.fetch();
