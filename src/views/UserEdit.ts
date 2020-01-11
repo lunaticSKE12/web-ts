5e247608a9235eecf7ea02d1ab65deabc4c5c3c4
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-11 12:40:28 +0700

    View nesting

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,3 +1,5 @@
+import { UserShow } from './UserShow';
+import { UserForm } from './UserForm';
 import { User, UserProps } from './../models/User';
 import { View } from './View';
 
@@ -7,6 +9,11 @@ export class UserEdit extends View<User, UserProps> {
       userShow: '.user-show',
       userForm: '.user-form'
     };
+  }
+
+  onRender(): void {
+    new UserShow(this.regions.userShow, this.model).render();
+    new UserForm(this.regions.userForm, this.model).render();
   }
   template(): string {
     return `
